7d4ec0fc4ad4d73836a682262a1465ba06a68610
Marvin T <contact@marvinkturner.com>
2020-10-12 23:59:25 -0400

    feat: add dark style js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+// Dark mode styles
+function darkMode() {
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255/ 50%)';
+}
 
 // Switch theme dynamically
 function switchTheme(event) {
