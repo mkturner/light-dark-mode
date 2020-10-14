a022471da1e03b308cca3472ff51c0dcf6071043
Marvin T <contact@marvinkturner.com>
2020-10-13 23:52:06 -0400

    feat: finish full light/dark capability

--- script.js
+++ script.js
@@ -10,14 +10,32 @@ const textBox = document.getElementById('text-box');
 function darkMode() {
   nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
   textBox.style.backgroundColor = 'rgb(255 255 255/ 50%)';
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  image1.src = 'img/undraw_proud_coder_dark.svg';
+  image2.src = 'img/undraw_feeling_proud_dark.svg';
+  image3.src = 'img/undraw_conceptual_idea_dark.svg';
+}
+
+// Light mode styles
+function lightMode() {
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = 'rgb(0 0 0/ 50%)';
+  toggleIcon.children[0].textContent = 'Light Mode';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  image1.src = 'img/undraw_proud_coder_light.svg';
+  image2.src = 'img/undraw_feeling_proud_light.svg';
+  image3.src = 'img/undraw_conceptual_idea_light.svg';
 }
 
 // Switch theme dynamically
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    darkMode();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    lightMode();
   }
 }
 
